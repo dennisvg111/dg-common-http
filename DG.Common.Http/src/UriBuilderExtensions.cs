1a938165cd100204af34379a15f5618129fa3f88
dennisvg111 <dennisvg111@gmail.com>
2023-06-09 17:48:33 +0200

    Add UriBuilderExtensions with tests

---
+++ DG.Common.Http/src/UriBuilderExtensions.cs
@@ -0,0 +1,112 @@
+﻿using DG.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DG.Common.Http
+{
+    /// <summary>
+    /// This class provides extension methods for <see cref="UriBuilder"/>.
+    /// </summary>
+    public static class UriBuilderExtensions
+    {
+        /// <summary>
+        /// Gets the current Query part of a <see cref="UriBuilder"/> as a collection of name value pairs.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IEnumerable<KeyValuePair<string, string>> GetQueryParts(this UriBuilder builder)
+        {
+            var query = builder.Query;
+            query = query.TrimStart('?');
+            var currentParts = query.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var part in currentParts)
+            {
+                if (!part.Contains("="))
+                {
+                    yield return new KeyValuePair<string, string>(Uri.UnescapeDataString(part), null);
+                    continue;
+                }
+                var name = part.Substring(0, part.IndexOf("="));
+                var value = part.Substring(part.IndexOf("=") + 1);
+                yield return new KeyValuePair<string, string>(Uri.UnescapeDataString(name), Uri.UnescapeDataString(value));
+            }
+        }
+
+        /// <summary>
+        /// Sets the Query part of a <see cref="UriBuilder"/> based on a collection of name value pairs.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="queryParts"></param>
+        public static void SetQueryParts(this UriBuilder builder, IEnumerable<KeyValuePair<string, string>> queryParts)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var part in queryParts)
+            {
+                sb.Append(Uri.EscapeDataString(part.Key));
+                if (part.Value != null)
+                {
+                    sb.Append("=" + Uri.EscapeDataString(part.Value));
+                }
+                sb.Append("&");
+            }
+            builder.Query = sb.ToString().TrimEnd('&');
+        }
+
+        /// <summary>
+        /// Adds a new value to the current query. Returns the current <see cref="UriBuilder"/>, to allow this method to be called using a fluent pattern.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static UriBuilder WithQuery(this UriBuilder builder, string value)
+        {
+            return WithQuery(builder, value, null);
+        }
+
+        /// <summary>
+        /// Adds a new name value pair to the current query. Returns the current <see cref="UriBuilder"/>, to allow this method to be called using a fluent pattern.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static UriBuilder WithQuery(this UriBuilder builder, string name, string value)
+        {
+            return WithQueryParts(builder, new KeyValuePair<string, string>[] { new KeyValuePair<string, string>(name, value) });
+        }
+
+        /// <summary>
+        /// Adds a collection of name value pairs to the current query. Returns the current <see cref="UriBuilder"/>, to allow this method to be called using a fluent pattern.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="newQueryParts"></param>
+        /// <returns></returns>
+        public static UriBuilder WithQueryParts(this UriBuilder builder, IEnumerable<KeyValuePair<string, string>> newQueryParts)
+        {
+            List<KeyValuePair<string, string>> parts = builder.GetQueryParts().ToList();
+            foreach (var kv in newQueryParts)
+            {
+                AddParts(parts, kv.Key, kv.Value);
+            }
+            builder.SetQueryParts(parts);
+            return builder;
+        }
+
+        private static void AddParts(List<KeyValuePair<string, string>> oldQueryParts, string name, string value)
+        {
+            if (name == null)
+            {
+                name = value;
+                value = null;
+            }
+            ThrowIf.Parameter.IsNull(name, nameof(name), "Name and value of query cannot both be null.");
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name), "Name and value of query cannot both be null.");
+            }
+            oldQueryParts.Add(new KeyValuePair<string, string>(name, value));
+        }
+    }
+}
---
+++ DG.Common.Http/tests/UriBuilderExtensionsTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DG.Common.Http.Tests
+{
+    public class UriBuilderExtensionsTests
+    {
+        [Fact]
+        public void GetQueryParts_ReturnsList()
+        {
+            UriBuilder builder = new UriBuilder("https://www.test.com/login.aspx?test=123&test=abc");
+
+            var queryParts = builder.GetQueryParts();
+
+            Assert.NotNull(queryParts);
+            Assert.Equal(2, queryParts.Count());
+            Assert.Contains(queryParts, (kv) => kv.Key == "test" && kv.Value == "123");
+            Assert.Contains(queryParts, (kv) => kv.Key == "test" && kv.Value == "abc");
+        }
+
+        [Fact]
+        public void GetQueryParts_Unescapes()
+        {
+            UriBuilder builder = new UriBuilder("https://www.test.com/login.aspx?test1=%25&test2=%3D");
+
+            var queryParts = builder.GetQueryParts();
+
+            Assert.NotNull(queryParts);
+            Assert.NotEmpty(queryParts);
+            Assert.Contains(queryParts, (kv) => kv.Key == "test1" && kv.Value == "%");
+            Assert.Contains(queryParts, (kv) => kv.Key == "test2" && kv.Value == "=");
+        }
+
+        [Fact]
+        public void GetQueryParts_ReturnsSingle()
+        {
+            UriBuilder builder = new UriBuilder("https://www.test.com/login.aspx?test=123");
+
+            var queryParts = builder.GetQueryParts();
+
+            Assert.NotNull(queryParts);
+            Assert.Single(queryParts);
+            Assert.Contains(queryParts, (kv) => kv.Key == "test" && kv.Value == "123");
+        }
+
+        [Fact]
+        public void GetQueryParts_ReturnsEmpty()
+        {
+            UriBuilder builder = new UriBuilder("https://www.test.com/login.aspx");
+
+            var queryParts = builder.GetQueryParts();
+
+            Assert.NotNull(queryParts);
+            Assert.Empty(queryParts);
+        }
+
+        [Fact]
+        public void SetQueryParts_Replaces()
+        {
+            UriBuilder builder = new UriBuilder("https://www.test.com/login.aspx?unused=test");
+            var queryParts = new KeyValuePair<string, string>[]
+            {
+                new KeyValuePair<string, string>("test", "123"),
+                new KeyValuePair<string, string>("test", "abc")
+            };
+
+            builder.SetQueryParts(queryParts);
+            string uri = builder.Uri.ToString();
+
+            Assert.Equal("https://www.test.com/login.aspx?test=123&test=abc", uri);
+        }
+
+        [Fact]
+        public void SetQueryParts_Escapes()
+        {
+            UriBuilder builder = new UriBuilder("https://www.test.com/login.aspx");
+            var queryParts = new KeyValuePair<string, string>[]
+            {
+                new KeyValuePair<string, string>("test", "%25"),
+                new KeyValuePair<string, string>("test", "123="),
+                new KeyValuePair<string, string>("test", "a&bc")
+            };
+
+            builder.SetQueryParts(queryParts);
+            string uri = builder.Uri.ToString();
+
+            Assert.Equal("https://www.test.com/login.aspx?test=%2525&test=123%3D&test=a%26bc", uri);
+        }
+
+        [Fact]
+        public void WithQueryParts_Adds()
+        {
+            UriBuilder builder = new UriBuilder("https://www.test.com/login.aspx?used=test");
+            var queryParts = new KeyValuePair<string, string>[]
+            {
+                new KeyValuePair<string, string>("test", null),
+                new KeyValuePair<string, string>("test", "abc")
+            };
+
+            builder.WithQueryParts(queryParts);
+            string uri = builder.Uri.ToString();
+
+            Assert.Equal("https://www.test.com/login.aspx?used=test&test&test=abc", uri);
+        }
+    }
+}
