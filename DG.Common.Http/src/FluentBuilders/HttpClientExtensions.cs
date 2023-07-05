c24e415f585bc05732d76e1a8670cb55c5cde808
dennisvg111 <dennisvg111@gmail.com>
2023-07-05 11:48:02 +0200

    Fix naming

--- DG.Common.Http/src/FluentBuilders/HttpClientExtensions.cs
+++ DG.Common.Http/src/FluentBuilders/HttpClientExtensions.cs
@@ -11,7 +11,7 @@ namespace DG.Common.Http.FluentBuilders
         /// <param name="client">The client used to send a <see cref="HttpRequestMessage"/>.</param>
         /// <param name="messageBuilder">The builder used to create a <see cref="HttpRequestMessage"/>.</param>
         /// <returns>Returns a <see cref="Task{TResult}"/> object representing the asynchronous operation.</returns>
-        public static async Task<HttpResponseMessage> SendMessage(this HttpClient client, HttpRequestMessageBuilder messageBuilder)
+        public static async Task<HttpResponseMessage> SendMessageAsync(this HttpClient client, HttpRequestMessageBuilder messageBuilder)
         {
             var request = messageBuilder.Message;
 
