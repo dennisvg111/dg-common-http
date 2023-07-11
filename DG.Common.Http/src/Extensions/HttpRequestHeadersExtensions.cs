15242e35ffd25dc07d7a3c688e78dbc94b4caca1
dennisvg111 <dennisvg111@gmail.com>
2023-07-11 10:46:16 +0200

    Add HttpRequestHeaderExtensions

---
+++ DG.Common.Http/src/Extensions/HttpRequestHeadersExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Headers;
+
+namespace DG.Common.Http.Extensions
+{
+    public static class HttpRequestHeadersExtensions
+    {
+        public static void AddOrReplace(this HttpRequestHeaders headers, string name, string value)
+        {
+            if (headers.Contains(name))
+            {
+                headers.Remove(name);
+            }
+            headers.Add(name, value);
+        }
+    }
+}
