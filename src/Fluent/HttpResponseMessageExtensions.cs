760fad8a0add4abaefd3c27bfad998ab55b65d40
dennisvg111 <dennisvg111@gmail.com>
2023-10-18 14:03:20 +0200

    Add extension methods used to ease deserializing responses (#5)
    * Bump version to 0.5.0

---
+++ src/Fluent/HttpResponseMessageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DG.Common.Http.Fluent
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="HttpResponseMessage"/>
+    /// </summary>
+    public static class HttpResponseMessageExtensions
+    {
+        /// <summary>
+        /// Deserializes the JSON content of this response to the specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static async Task<T> DeserializeResponseAsync<T>(this HttpResponseMessage response)
+        {
+            var result = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(result);
+        }
+    }
+}
