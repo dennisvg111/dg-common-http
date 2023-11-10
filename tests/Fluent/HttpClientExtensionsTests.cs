8b07c3def42e48717b9a1b082c7be6de8b93f63c
dennisvg111 <dennisvg111@gmail.com>
2023-11-10 09:41:10 +0100

    Add a test to see if HttpClientExtensions uses ConfigureAwait(false)

---
+++ tests/Fluent/HttpClientExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using DG.Common.Http.Fluent;
+using DG.Common.Http.Testing;
+using DG.Common.Http.Tests.TestUtilities;
+using NSubstitute;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DG.Common.Http.Tests.Fluent
+{
+    public class HttpClientExtensionsTests
+    {
+        private HttpClient CreateClient()
+        {
+            var mockedHandler = Substitute.For<ISimpleMockHandler>();
+            mockedHandler.GetResponse(Arg.Any<SimpleMockRequest>()).Returns(SimpleMockResponse.Ok());
+            return mockedHandler.CreateNewClient();
+        }
+
+        [Fact]
+        public void SendAsync_UsesConfigureAwaitFalse()
+        {
+            var client = CreateClient();
+            var request = FluentRequest.Get.To("https://www.test.com");
+
+            Func<Task<HttpResponseMessage>> task = () => HttpClientExtensions.SendAsync(client, request);
+
+            ContextAsserts.AssertContextSwitched(task);
+        }
+    }
+}
---
+++ tests/TestUtilities/ContextAsserts.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DG.Common.Http.Tests.TestUtilities
+{
+    /// <summary>
+    /// This class provides functionality to test if ConfigureAwait(false) is used when awaiting asynchronous functions.
+    /// </summary>
+    public static class ContextAsserts
+    {
+        private static readonly object _lock = new object();
+
+        /// <summary>
+        /// Asserts that the given task does not continue on the curent context after awaiting.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        public static void AssertContextSwitched<T>(Func<Task<T>> task)
+        {
+            var context = TestSynchronizationContext.Instance;
+
+            lock (_lock)
+            {
+                var old = SynchronizationContext.Current;
+                try
+                {
+                    SynchronizationContext.SetSynchronizationContext(context);
+                    context.Reset();
+
+                    task().Wait();
+
+                    Assert.True(context.ContextSwitched, $"Task continued on the same context it started, use ConfigureAwait(false) instead.");
+                }
+                finally
+                {
+                    SynchronizationContext.SetSynchronizationContext(old);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Asserts that the given task does not continue on the curent context after awaiting.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="task"></param>
+        public static void AssertContextSwitched(Func<Task> task)
+        {
+            AssertContextSwitched(() => task().ContinueWith((t) => Task.FromResult(1)));
+        }
+
+        private class TestSynchronizationContext : SynchronizationContext
+        {
+            private static readonly TestSynchronizationContext _instance = new TestSynchronizationContext();
+            public static TestSynchronizationContext Instance => _instance;
+
+            private bool _postCalled;
+
+            public bool ContextSwitched => !_postCalled;
+
+            private TestSynchronizationContext()
+            {
+            }
+
+            public override void Post(SendOrPostCallback d, object state)
+            {
+                _postCalled = true;
+                base.Post(d, state);
+            }
+
+            public void Reset()
+            {
+                _postCalled = false;
+            }
+        }
+    }
+}
