d22a8b96a28623641184495f67111139043cb4a4
dennisvg111 <dennisvg111@gmail.com>
2023-11-16 10:09:48 +0100

    Add tests for IsAuthorizedAsync

--- tests/Authorization/OAuth2/OAuthFlowHandlerTests.cs
+++ tests/Authorization/OAuth2/OAuthFlowHandlerTests.cs
@@ -23,7 +23,7 @@ namespace DG.Common.Http.Tests.Authorization.OAuth2
             TestOAuthorizationServer.Instance.StartAuthorization(request);
             TestOAuthorizationServer.Instance.AcceptAuthorization(request, out string code);
 
-            await flow.AuthorizationCallback(code);
+            await flow.AuthorizationCallbackAsync(code);
 
             repository.Received(2).Save(Arg.Any<OAuthData>());
 
--- tests/Authorization/OAuth2/OAuthFlowTests.cs
+++ tests/Authorization/OAuth2/OAuthFlowTests.cs
@@ -2,6 +2,9 @@
 using DG.Common.Http.Authorization.OAuth2;
 using DG.Common.Http.Authorization.OAuth2.Data;
 using DG.Common.Http.Authorization.OAuth2.Exceptions;
+using DG.Common.Http.Authorization.OAuth2.Interfaces;
+using DG.Common.Threading;
+using NSubstitute;
 using System;
 using System.Threading.Tasks;
 using Xunit;
@@ -10,6 +13,70 @@ namespace DG.Common.Http.Tests.Authorization.OAuth2
 {
     public class OAuthFlowTests
     {
+        [Fact]
+        public async Task IsAuthorizedAsync_NotCompleted_ReturnsFalse()
+        {
+            var logic = Substitute.For<IOAuthLogic>();
+            var data = new OAuthData()
+            {
+                State = Guid.NewGuid().ToString(),
+                Scopes = new string[0],
+                CallBackUri = new System.Uri("https://www.test.com"),
+                Started = DateTimeOffset.UtcNow
+            };
+            var flow = new OAuthFlow(logic, data);
+
+            var isAuthorized = await flow.IsAuthorizedAsync();
+
+            Assert.False(isAuthorized);
+        }
+
+        [Fact]
+        public async Task IsAuthorizedAsync_Valid_ReturnsFalse()
+        {
+            var logic = Substitute.For<IOAuthLogic>();
+            var data = new OAuthData()
+            {
+                State = Guid.NewGuid().ToString(),
+                Scopes = new string[0],
+                CallBackUri = new System.Uri("https://www.test.com"),
+                Started = DateTimeOffset.UtcNow,
+                AccessToken = Guid.NewGuid().ToString(),
+                ValidUntill = DateTimeOffset.UtcNow.AddDays(1),
+                RefreshToken = Guid.NewGuid().ToString()
+            };
+            var flow = new OAuthFlow(logic, data);
+
+            var isAuthorized = await flow.IsAuthorizedAsync();
+
+            Assert.True(isAuthorized);
+        }
+
+        [Fact]
+        public async Task IsAuthorizedAsync_InValid_CallsRefresh()
+        {
+            var logic = Substitute.For<IOAuthLogic>();
+            Func<TaskResult<OAuthToken>> resultFunc = () => TaskResult.Success(new OAuthToken("access-token", DateTimeOffset.UtcNow.AddDays(1), "refresh-token"));
+            logic.RefreshTokenAsync(Arg.Any<string>()).Returns(Task.FromResult(resultFunc()));
+            string refreshToken = "refresh-" + Guid.NewGuid();
+            var data = new OAuthData()
+            {
+                State = Guid.NewGuid().ToString(),
+                Scopes = new string[0],
+                CallBackUri = new System.Uri("https://www.test.com"),
+                Started = DateTimeOffset.UtcNow,
+                AccessToken = Guid.NewGuid().ToString(),
+                ValidUntill = DateTimeOffset.UtcNow.AddDays(-1),
+                RefreshToken = refreshToken
+            };
+            var flow = new OAuthFlow(logic, data);
+
+            var isAuthorized = await flow.IsAuthorizedAsync();
+
+            await logic.Received(1).RefreshTokenAsync(Arg.Is(refreshToken));
+            Assert.True(isAuthorized);
+        }
+
         [Fact]
         public async Task GetAuthorizationHeaderAsync_NewStarted_Throws()
         {
