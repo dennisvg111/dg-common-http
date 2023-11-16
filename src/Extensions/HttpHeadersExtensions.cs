299a4a9f9cb61ba5e8ba9cccdea733bc5a399948
dennisvg111 <dennisvg111@gmail.com>
2023-11-16 15:15:32 +0100

    Update documentation (#23)
    * Split HttpHeadersExtensions between fluent or not, and add documentation
    
    * Allow direct usage of OAuthFlow in FluentRequest
    
    * Update version to 1.2.13
    
    * Update README

--- src/Extensions/HttpHeadersExtensions.cs
+++ src/Extensions/HttpHeadersExtensions.cs
@@ -1,10 +1,18 @@
-﻿using DG.Common.Http.Fluent;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 
 namespace DG.Common.Http.Extensions
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="HttpHeaders"/>.
+    /// </summary>
     public static class HttpHeadersExtensions
     {
+        /// <summary>
+        /// Adds the specified header and its value into the <see cref="HttpHeaders"/> collection, replacing any existing header with the same name.
+        /// </summary>
+        /// <param name="headers"></param>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
         public static void AddOrReplace(this HttpHeaders headers, string name, string value)
         {
             if (headers.Contains(name))
@@ -12,11 +20,6 @@ namespace DG.Common.Http.Extensions
                 headers.Remove(name);
             }
             headers.Add(name, value);
-        }
-
-        public static void AddOrReplace(this HttpHeaders headers, FluentHeader header)
-        {
-            headers.AddOrReplace(header.Name, header.Value);
         }
     }
 }
---
+++ src/Fluent/HttpHeadersExtensions.cs
@@ -0,0 +1,42 @@
+﻿using DG.Common.Http.Extensions;
+using System.Net.Http.Headers;
+
+namespace DG.Common.Http.Fluent
+{
+    /// <summary>
+    /// Provides extension methods with <see cref="FluentHeader"/> parameters for <see cref="HttpHeaders"/>.
+    /// </summary>
+    public static class HttpHeadersExtensions
+    {
+        /// <summary>
+        /// Returns a value that indicates whether the specified <paramref name="header"/> was added to the <see cref="HttpHeaders"/> without validating the provided information.
+        /// </summary>
+        /// <param name="headers"></param>
+        /// <param name="header"></param>
+        /// <returns>Returns <see langword="true"/> if the specified <paramref name="header"/> could be added to the collection; otherwise <see langword="false"/>.</returns>
+        public static bool TryAddWithoutValidation(this HttpHeaders headers, FluentHeader header)
+        {
+            return headers.TryAddWithoutValidation(header.Name, header.Value);
+        }
+
+        /// <summary>
+        /// Adds the specified <paramref name="header"/> into the <see cref="HttpHeaders"/> collection.
+        /// </summary>
+        /// <param name="headers"></param>
+        /// <param name="header"></param>
+        public static void Add(this HttpHeaders headers, FluentHeader header)
+        {
+            headers.Add(header.Name, header.Value);
+        }
+
+        /// <summary>
+        /// Adds the specified <paramref name="header"/> into the <see cref="HttpHeaders"/> collection, replacing any existing header with the same name.
+        /// </summary>
+        /// <param name="headers"></param>
+        /// <param name="header"></param>
+        public static void AddOrReplace(this HttpHeaders headers, FluentHeader header)
+        {
+            headers.AddOrReplace(header.Name, header.Value);
+        }
+    }
+}
