2cf6b3e28330faa1806a0dae6f98b28a6056ae3a
dennisvg111 <dennisvg111@gmail.com>
2023-12-01 17:42:09 +0100

    Improve HttpClientProvider and move to main DG.Common.Http namespace

---
+++ src/HttpClientProvider.cs
@@ -0,0 +1,55 @@
+﻿using DG.Common.Caching;
+using System;
+using System.Net.Http;
+using System.Runtime.Caching;
+
+namespace DG.Common.Http
+{
+    /// <summary>
+    /// This class provides a custom way to create instances of <see cref="HttpClient"/> to prevent socket exhaustion and DNS caching problems.
+    /// </summary>
+    public static class HttpClientProvider
+    {
+        //We cache clients to prevent socket exhaustion, and cache it only for 5 minutes to prevent DNS problems.
+        private const string _cacheName = "DG.Common.Http " + nameof(HttpClientProvider) + " Cache";
+        private static readonly TypedCache<HttpClient> _cache = new TypedCache<HttpClient>(
+            ExpirationPolicy.ForAbsoluteExpiration(TimeSpan.FromMinutes(5)),
+            new MemoryCache(_cacheName)
+        );
+
+        /// <summary>
+        /// <para>Gets a <see cref="HttpClient"/> instance constructed by the given <see cref="HttpClientSettings"/>.</para>
+        /// <para>Note that this may return the same instance multiple times if no new instance is needed.</para>
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public static HttpClient ClientForSettings(HttpClientSettings settings)
+        {
+            return _cache.GetOrCreate(settings.GetCacheId(), () => CreateNewClientForSettings(settings));
+        }
+
+        /// <summary>
+        /// Returns a new instance of <see cref="HttpClient"/> for the given <see cref="HttpClientSettings"/>.
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public static HttpClient CreateNewClientForSettings(HttpClientSettings settings)
+        {
+            HttpClientHandler handler = new HttpClientHandler
+            {
+                UseCookies = settings.UseCookies,
+                AllowAutoRedirect = settings.MaxRedirects > 0
+            };
+            if (settings.MaxRedirects > 0)
+            {
+                handler.MaxAutomaticRedirections = settings.MaxRedirects;
+            }
+
+            return new HttpClient(handler, true)
+            {
+                BaseAddress = settings.BaseAddress,
+                Timeout = settings.MaxTimeout
+            };
+        }
+    }
+}
---
+++ src/HttpClientSettings.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Net.Http;
+
+namespace DG.Common.Http
+{
+    /// <summary>
+    /// Provides a custom constructor for the <see cref="HttpClient"/> class.
+    /// </summary>
+    public sealed class HttpClientSettings
+    {
+        /// <summary>
+        /// The default maximum number of redirects that a request will follow automatically.
+        /// </summary>
+        public const int DefaultAutomaticRedirectLimit = 50;
+
+        /// <summary>
+        /// The default number of seconds (100) before the request times out.
+        /// </summary>
+        public readonly static TimeSpan DefaultTimeout = TimeSpan.FromSeconds(100);
+
+        private readonly Uri _baseAddress;
+
+        private int _maxRedirects;
+        private bool _useCookies;
+        private TimeSpan _maxTimeout;
+
+        internal Uri BaseAddress => _baseAddress;
+        internal int MaxRedirects => _maxRedirects;
+        internal TimeSpan MaxTimeout => _maxTimeout;
+        internal bool UseCookies => _useCookies;
+
+        private HttpClientSettings(Uri baseAddress)
+        {
+            _baseAddress = baseAddress;
+            _maxRedirects = DefaultAutomaticRedirectLimit;
+            _maxTimeout = TimeSpan.FromSeconds(100);
+        }
+
+        private void CopyTo(HttpClientSettings copy)
+        {
+            copy._maxRedirects = _maxRedirects;
+            copy._useCookies = _useCookies;
+            copy._maxTimeout = _maxTimeout;
+        }
+
+        private HttpClientSettings Copy()
+        {
+            var copy = new HttpClientSettings(_baseAddress);
+            CopyTo(copy);
+            return copy;
+        }
+
+        /// <summary>
+        /// Returns a copy of this <see cref="HttpClientSettings"/> where the maximum amount of automatic redirects is limited to <paramref name="maxRedirects"/>.
+        /// </summary>
+        /// <param name="maxRedirects"></param>
+        /// <returns></returns>
+        public HttpClientSettings LimitAutomaticRedirectsTo(int maxRedirects)
+        {
+            var copy = Copy();
+            copy._maxRedirects = maxRedirects;
+            return copy;
+        }
+
+        /// <summary>
+        /// Returns a copy of this <see cref="HttpClientSettings"/> where redirects will not be automatically followed.
+        /// </summary>
+        /// <returns></returns>
+        public HttpClientSettings WithoutRedirects()
+        {
+            return LimitAutomaticRedirectsTo(0);
+        }
+
+        /// <summary>
+        /// Returns a copy of this <see cref="HttpClientSettings"/> where <see cref="HttpClientHandler.CookieContainer"/> will be used to automatically apply and retrieve cookies.
+        /// </summary>
+        /// <returns></returns>
+        public HttpClientSettings WithoutCookies()
+        {
+            var copy = Copy();
+            copy._useCookies = false;
+            return copy;
+        }
+
+        /// <summary>
+        /// Returns a copy of this <see cref="HttpClientSettings"/> where <see cref="HttpClientHandler.CookieContainer"/> will NOT be used to automatically apply and retrieve cookies.
+        /// </summary>
+        /// <returns></returns>
+        public HttpClientSettings WithCookies()
+        {
+            var copy = Copy();
+            copy._useCookies = true;
+            return copy;
+        }
+
+        /// <summary>
+        /// Returns a copy of this <see cref="HttpClientSettings"/> where requests will timeout after <paramref name="timeout"/>.
+        /// </summary>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public HttpClientSettings WithTimeout(TimeSpan timeout)
+        {
+            var copy = Copy();
+            copy._maxTimeout = timeout;
+            return copy;
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="HttpClientSettings"/> without a base address.
+        /// </summary>
+        /// <returns></returns>
+        public static HttpClientSettings WithoutBaseAddress()
+        {
+            return new HttpClientSettings(null);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="HttpClientSettings"/> with the given <paramref name="baseAddress"/>.
+        /// </summary>
+        /// <param name="baseAddress"></param>
+        /// <returns></returns>
+        public static HttpClientSettings WithBaseAddress(Uri baseAddress)
+        {
+            return new HttpClientSettings(baseAddress);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="HttpClientSettings"/> with the given <paramref name="baseAddress"/>.
+        /// </summary>
+        /// <param name="baseAddress"></param>
+        /// <returns></returns>
+        public static HttpClientSettings WithBaseAddress(string baseAddress)
+        {
+            return new HttpClientSettings(new Uri(baseAddress));
+        }
+
+        /// <summary>
+        /// Returns a string to be used as a cache id. If two instances of <see cref="HttpClientSettings"/> return the same string, it is assumed they may result in the same <see cref="HttpClient"/>.
+        /// </summary>
+        /// <returns></returns>
+        internal string GetCacheId()
+        {
+            return $"{_baseAddress} - {_maxTimeout},{_maxRedirects},{_useCookies}";
+        }
+    }
+}
